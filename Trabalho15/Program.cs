8403ce7120edd9c8e675844b2544b1a9f75b1d09
adexpz <159590286+adexpz@users.noreply.github.com>
2025-06-15 22:18:10 -0300

    Update Program.cs

--- Trabalho15/Program.cs
+++ Trabalho15/Program.cs
@@ -1,8 +1,15 @@
-﻿decimal andrefelip, conta1, total;
-Console.Write("Digite o valor total da conta: ");
-decimal conta  = Convert.ToInt32(Console.ReadLine());
+decimal totalConta;
+decimal parteCarlos, parteAndre;
+int parteFelipe;
 
-andrefelip = conta / 3;
-int inteiroo  = (andrefelip)   
+Console.Write("Digite o valor total da conta R$ : ");
+totalConta = Convert.ToDecimal(Console.ReadLine());
 
+parteFelipe = (int)Math.Ceiling(totalConta / 3);
+decimal restante = totalConta - parteFelipe;
+parteCarlos = restante / 2;
+parteAndre = parteCarlos;
 
+        Console.WriteLine($"\nCarlos deve pagar: R$ {parteCarlos:F2}");
+        Console.WriteLine($"André deve pagar:  R$ {parteAndre:F2}");
+        Console.WriteLine($"Felipe deve pagar: R$ {parteFelipe},00");
